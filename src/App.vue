7b77cd79adddd6739c51dd0f14cb5b2c3045a726
xujianrj <keys203@gmail.com>
2017-05-27 10:25:32 +0800

    sample code

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,12 @@ import '../static/swiper-3.4.2.min.js'
 export default {
   name: 'app',
   mounted() {
+    //NOTE:sample code to prevent browser scroll.
+   // document.querySelector('body').addEventListener('touchmove', function (e) {
+     // if (document.querySelector('.m-select').contains(e.target)) {
+       // e.preventDefault();
+     // }
+    });
   }
 }
 </script>
