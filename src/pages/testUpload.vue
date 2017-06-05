be25ac6f6ef5f023b14a598143c31d0a9ff868ac
xujianrj <keys203@gmail.com>
2017-06-05 20:33:59 +0800

    update

--- src/pages/testUpload.vue
+++ src/pages/testUpload.vue
@@ -32,8 +32,8 @@
 //                console.log(res);
                 // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
             });
-            var url=location.href.split('#')[0];
-            axios.get('http://localhost:3333/wechat/get_signature?url='+url)
+            var domain=location.href.split('#')[0];//Note http://localhost:3333/
+            axios.get(domain+'wechat/get_signature?url='+domain)
                 .then(function (res) {
                     var data=res.data;
                     console.log(data);
@@ -45,7 +45,7 @@
 
                     wx.config({
                         debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-                        appId: 'wxa68c344b9d092e3b', // 必填，公众号的唯一标识
+                        appId: data.appId, // 必填，公众号的唯一标识
                         timestamp: data.timestamp, // 必填，生成签名的时间戳
                         nonceStr: data.nonceStr, // 必填，生成签名的随机串
                         signature: data.signature,// 必填，签名，见附录1
