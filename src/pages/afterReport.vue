6dd7a19b0eb0c7d6b82ac34daedd11e04d01cf4f
htt <htt6275@163.com>
2017-06-08 15:39:00 +0800

    third

--- src/pages/afterReport.vue
+++ src/pages/afterReport.vue
@@ -51,12 +51,16 @@
 				<img src="../assets/afterHou.png"/>
 			</div>
 			<img class="close" src="../assets/shopcarClose.png" @click='afterReportHidden = !afterReportHidden'/>
-			<p>
-			你所在的城市6月气温升高，空气中湿度增加，体内汗液无法通畅的发散出来，
-	    	人们就会感到胸闷、心悸，精神不振，全身乏力。易出现中暑、腮腺炎、水痘。
-	    	<span>根据你的体质和所在地区基本月物侯，我给你制定了一份你的养生调理方案，请点击查看。</span>
-	    	每两个月根据气候的变化，我会更新你的养生调理方案，你可以通过公众号到时查看，祝你健康！
-			</p>
+			<div class="reportDetailcontent">
+				<p>
+				你所在的城市6月气温升高，空气中湿度增加，体内汗液无法通畅的发散出来，<br/>
+		    	人们就会感到胸闷、心悸，精神不振，全身乏力。易出现中暑、腮腺炎、水痘。
+		    	根据你的体质和所在地区基本月物侯，我给你制定了一份你的养生调理方案，请点击查看
+		   		根据你的体质和所在地区基本月物侯，我给你制定了一份你的养生调理方案，请点击查看
+		   		根据你的体质和所在地区基本月物侯，我给你制定了一份你的养生调理方案，请点击查看
+		   		根据你的体质和所在地区基本月物侯，我给你制定了一份你的养生调理方案，请点击查看
+				</p>
+			</div>
 		</div>
 	  </div>
 	  <!--后天报告详情结束-->
@@ -272,17 +276,21 @@ export default {
 			top: rem(10rem);
 			right: rem(10rem);
 		}
-		p{
-			margin: rem(48rem) 8% 0;
-	  		line-height: rem(22rem);;
-	  		padding-bottom: rem(10rem);
-	  		font-size: $font14;
-	  		color: #505050;
-	  		border-bottom: 1px double #f4eade;
-	  		span{
-	  			display: inline-block;
-	  			margin: rem(10rem) 0;
-	  		}
+		.reportDetailcontent{
+			width: 100%;
+			height: rem(275rem);
+			margin-top:  rem(48rem);
+			border-bottom: 1px double #f4eade;
+			overflow: auto;
+			p{
+				margin:0 8%;
+		  		line-height: rem(22rem);;
+		  		padding-bottom: rem(10rem);
+		  		font-size: $font14;
+		  		color: #505050;
+		  		text-align: justify;
+	        text-justify: inter-ideograph;
+			}
 		}
 	}
 }
