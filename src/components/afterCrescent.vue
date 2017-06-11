2d5b4c227b951b78172382a7d7d705eeaea54296
xujianrj <keys203@gmail.com>
2017-06-11 11:31:01 +0800

    enable upload image.

--- src/components/afterCrescent.vue
+++ src/components/afterCrescent.vue
@@ -49,7 +49,8 @@
                 var that = this;
                 wx.chooseImage({
                     count: 1, // 默认9
-                    sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+//                    sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+                    sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有
                     sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
                     success: function (res) {
                         that.fingerImage = res.localIds[0];
@@ -67,7 +68,7 @@
                             });
                         }
                         that.isChosen = true;
-                        that.uploadPic();
+                        that.uploadPic(res.localIds[0].toString());
                     }
                 })
             },
@@ -88,9 +89,9 @@
                     });
                 }
             },
-            uploadPic(){
+            uploadPic(localId){
                 wx.uploadImage({
-                    localId: [this.fingerImage], // 需要上传的图片的本地ID，由chooseImage接口获得
+                    localId: localId, // 需要上传的图片的本地ID，由chooseImage接口获得
                     isShowProgressTips: 1, // 默认为1，显示进度提示
                     success: function (res) {
                         var serverId = res.serverId; // 返回图片的服务器端ID
