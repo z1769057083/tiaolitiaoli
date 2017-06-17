4b004264e976bf9153eab2e3cebdae6b2ce16f04
xujianrj <keys203@gmail.com>
2017-06-17 16:33:04 +0800

    update for build.

--- static/common.js
+++ static/common.js
@@ -80,7 +80,7 @@ export default {
         global.shareTitle=title;
         global.link=window.location.href;
         global.shareDesc=description;
-        let data=JSON.parse(localStorage.getItem(global.WechatSignature_Index))
+        var data=JSON.parse(localStorage.getItem(global.WechatSignature_Index))
         wx.config({
 //                        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: data.appId, // 必填，公众号的唯一标识
