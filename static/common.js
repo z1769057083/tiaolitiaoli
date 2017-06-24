2f5da5e33027ae68ee923a607555e2651545ebc7
xujianrj <keys203@gmail.com>
2017-06-24 14:00:35 +0800

    wechat share.

--- static/common.js
+++ static/common.js
@@ -83,6 +83,7 @@ export default {
         global.link=href;
         global.shareDesc=description;
         var data=JSON.parse(localStorage.getItem(global.WechatSignature_Index))
+        if(typeof (data)=='undefined'||data==null){return;}
         wx.config({
 //                        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: data.appId, // 必填，公众号的唯一标识
