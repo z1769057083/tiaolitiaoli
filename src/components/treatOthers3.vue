0d4eb7c8101c7164ab9d8e3f4f6a728a387a696f
xujianrj <keys203@gmail.com>
2017-06-24 16:52:23 +0800

    actors calculator1

--- src/components/treatOthers3.vue
+++ src/components/treatOthers3.vue
@@ -4,41 +4,41 @@
 	  		<dl>
 	  			<dt>为人和顺</dt>
 	  			<dd>
-	  				<p :class="{'active': section3.gentle === 0}" @click="change_active(0,'1','gentle',$event)">是</p>
-	  				<p :class="{'active': section3.gentle === 1}" @click="change_active(1,'1','gentle',$event)">不是</p>
-	  				<p :class="{'active': section3.gentle === 2}" @click="change_active(2,'1','gentle',$event)">不确定</p>
+	  				<p :class="{'active': section3.gentle === 1}" @click="change_active(1,'1','gentle',$event)">是</p>
+	  				<p :class="{'active': section3.gentle === 0}" @click="change_active(0,'1','gentle',$event)">不是</p>
+	  				<p :class="{'active': section3.gentle === -1}" @click="change_active(-1,'1','gentle',$event)">不确定</p>
 	  			</dd>
 	  		</dl>
 	  	  	<dl>
 	  			<dt>嫉恶如仇</dt>
 	  			<dd>
-	  				<p :class="{'active': section3.evil === 0}" @click="change_active(0,'2','evil',$event)">是</p>
-	  				<p :class="{'active': section3.evil === 1}" @click="change_active(1,'2','evil',$event)">不是</p>
-	  				<p :class="{'active': section3.evil === 2}" @click="change_active(2,'2','evil',$event)">不确定</p>
+	  				<p :class="{'active': section3.evil === 1}" @click="change_active(1,'2','evil',$event)">是</p>
+	  				<p :class="{'active': section3.evil === 0}" @click="change_active(0,'2','evil',$event)">不是</p>
+	  				<p :class="{'active': section3.evil === -1}" @click="change_active(-1,'2','evil',$event)">不确定</p>
 	  			</dd>
 	  		</dl>	
 	  		<dl>
 	  			<dt>处事灵活</dt>
 	  			<dd>
-	  				<p :class="{'active': section3.flexible === 0}" @click="change_active(0,'3','flexible',$event)">是</p>
-	  				<p :class="{'active': section3.flexible === 1}" @click="change_active(1,'3','flexible',$event)">不是</p>
-	  				<p :class="{'active': section3.flexible === 2}" @click="change_active(2,'3','flexible',$event)">不确定</p>
+	  				<p :class="{'active': section3.flexible === 1}" @click="change_active(1,'3','flexible',$event)">是</p>
+	  				<p :class="{'active': section3.flexible === 0}" @click="change_active(0,'3','flexible',$event)">不是</p>
+	  				<p :class="{'active': section3.flexible === -1}" @click="change_active(-1,'3','flexible',$event)">不确定</p>
 	  			</dd>
 	  		</dl>
 	  		<dl>
 	  			<dt>身材魁梧</dt>
 	  			<dd>
-	  				<p :class="{'active': section3.burly === 0}" @click="change_active(0,'4','burly',$event)">是</p>
-	  				<p :class="{'active': section3.burly === 1}" @click="change_active(1,'4','burly',$event)">不是</p>
-	  				<p :class="{'active': section3.burly === 2}" @click="change_active(2,'4','burly',$event)">不确定</p>
+	  				<p :class="{'active': section3.burly === 1}" @click="change_active(1,'4','burly',$event)">是</p>
+	  				<p :class="{'active': section3.burly === 0}" @click="change_active(0,'4','burly',$event)">不是</p>
+	  				<p :class="{'active': section3.burly === -1}" @click="change_active(-1,'4','burly',$event)">不确定</p>
 	  			</dd>
 	  		</dl>
 	  		<dl>
 	  			<dt>原则性强</dt>
 	  			<dd>
-	  				<p :class="{'active': section3.principle === 0}" @click="change_active(0,'5','principle',$event)">是</p>
-	  				<p :class="{'active': section3.principle === 1}" @click="change_active(1,'5','principle',$event)">不是</p>
-	  				<p :class="{'active': section3.principle === 2}" @click="change_active(2,'5','principle',$event)">不确定</p>
+	  				<p :class="{'active': section3.principle === 1}" @click="change_active(1,'5','principle',$event)">是</p>
+	  				<p :class="{'active': section3.principle === 0}" @click="change_active(0,'5','principle',$event)">不是</p>
+	  				<p :class="{'active': section3.principle === -1}" @click="change_active(-1,'5','principle',$event)">不确定</p>
 	  			</dd>
 	  		</dl>
 	  	</div>
