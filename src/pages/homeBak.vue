65f78d84ff29bcfc375accbdde698d96f3ce50f7
xujianrj <keys203@gmail.com>
2017-06-27 09:47:11 +0800

    update home page layout

--- src/pages/homeBak.vue
+++ src/pages/homeBak.vue
@@ -7,17 +7,24 @@
             <div class='i-main'>
                 <h3 class='i-maintop'>体质检测</h3>
                 <div class='i-maintext'>
-                    本测试由汉古科技各位教授多年潜心研究和临床观察，利用《皇帝内经》等传统中医理论结合人工智能技术创立的体质辨析研究模型；完成测试后，您将了解到您的体质详情，并获得相应的个性化的解决方法。
+                    本测试由林博士、胡大夫两位教授多年潜心研究和临床观察，利用《皇帝内经》等传统中医理论结合人工智能技术创立的体质辨析研究模型；完成测试后，您将了解到您的体质详情，并获得相应的个性化的解决方法。
                 </div>
                 <div class='i-maintext'>本次测试大约需要10分钟的时间填写。</div>
                 <div class='i-maincen'>
                     <img class="i-mline" src="../assets/indexline.png"/>
                     <img class="i-micon" src="../assets/index.png">
                 </div>
-                <div class="r-mbotbtn">
-                    <router-link to='/message'>
-                        <div class="r-mbotleft"><img src="../assets/reportleft.png"/></div>
-                    </router-link>
+                <div class='i-maindocter i-maindocter1'>
+                    <span>林博士</span>
+                    <div class="i-maintest" @click="tab(0)">
+                        <img src="../assets/indextest.png">
+                    </div>
+                    <div class='i-mainportrait'><img src="../assets/indexheadportrait.png"></div>
+                </div>
+                <div class='i-maindocter'>
+                    <span>胡大夫</span>
+                    <div class='i-maintest' @click="tab(1)"><img src="../assets/indextest.png"></div>
+                    <div class='i-mainportrait'><img src="../assets/indexheadportrait1.png"></div>
                 </div>
                 <div class='i-maintext i-maintip i-maintip1'>温馨提示：<br>本测试所提及的内容仅作为生活保健的咨询建议。</div>
             </div>
@@ -175,25 +182,6 @@
                 margin-top: rem(30rem);
                 color: #c59a6f;
                 font-weight: bold;
-            }
-            .r-mbotbtn {
-                width: 84%;
-                margin: rem(20rem) 0 0 8%;
-                overflow: hidden;
-                padding-bottom: rem(10rem);
-
-                .r-mbotleft {
-                    width: 47.6%;
-                    height: rem(50rem);
-                    margin-left: 26.2%;
-
-                    img {
-                        width: 100%;
-                        height: 100%;
-                    }
-
-                }
-
             }
 
         }
