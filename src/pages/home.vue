830bb7dbfd4a690b3d3de876fff0797882f91339
xujianrj <keys203@gmail.com>
2017-06-29 11:22:18 +0800

    update home page.

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -5,27 +5,22 @@
                 <div class='i-headbottext' style="display: none;">你是第{{visitorCount}}位测试者</div>
             </div>
             <div class='i-main'>
-                <h3 class='i-maintop'>体质检测</h3>
+                <h3 class='i-maintop'><img src="../assets/rmattribute.png" alt="" />体质检测<img src="../assets/rmattribute.png" alt="" /></h3>
                 <div class='i-maintext'>
-                    本测试由汉古科技各位教授多年潜心研究和临床观察，利用《皇帝内经》等传统中医理论结合人工智能技术创立的体质辨析研究模型；完成测试后，您将了解到您的体质详情，并获得相应的个性化的解决方法。
+                    本测试由汉古科技多位教授多年潜心研究，利用《皇帝内经》等传统中医理论结合人工智能技术创立的体质辨析研究模型；完成测试后，您将了解到您的体质详情，并获得相应的个性化的解决方法。
                 </div>
                 <div class='i-maintext'>本次测试大约需要10分钟的时间填写。</div>
                 <div class='i-maincen'>
                     <img class="i-mline" src="../assets/indexline.png"/>
                     <img class="i-micon" src="../assets/index.png">
                 </div>
-                <div class="r-mbotbtn">
-                    <router-link to='/message'>
-                        <div class="r-mbotleft"><img src="../assets/reportleft.png"/></div>
-                    </router-link>
-                </div>
-                <div class='i-maintext i-maintip i-maintip1'>温馨提示：<br>本测试所提及的内容仅作为生活保健的咨询建议。</div>
+                <div class="i-mainTest" @click='tab'></div>
+                <div class='i-maintip'>温馨提示：<br />本测试所提及的内容仅作为生活保健的咨询建议。</div>
             </div>
         </div>
     </div>
 </template>
 <script>
-
     import Toast from '@/packages/toast'
     import axios from 'axios'
     import api from '../api/api'
@@ -46,19 +41,15 @@
                     });
                 }
             },
-            tab(index) {
+            tab() {
                 if (!window.localStorage) {
                     return false;
                 } else {
-                    let doctor1 = { 'name': "林博士", 'img': "../static/images/indexheadportrait.png" },
-                        doctor2 = { 'name': "胡大夫", 'img': "../static/images/indexheadportrait1.png" }
-                    let doctors = [doctor1, doctor2];
-                    let currentDoctor = doctors[index];
-                    let obj_arr = JSON.stringify(currentDoctor)
+                    let doctor1 = { 'name': "林博士", 'img': "../static/images/indexheadportrait.png" }     
+                    let obj_arr = JSON.stringify(doctor1)
                     localStorage.setItem(Doctor_Name_Key, obj_arr);
-                    console.log(obj_arr)
                 }
-                this.$router.push({ path: '/message', query: { questionSection: XianTianSectionType } })
+                this.$router.push({ path: '/message'})
             }
         },
         mounted() {
@@ -86,14 +77,25 @@
         background-size: 100% 100%;
 
         .i-main {
-            margin-top: rem(18rem);
-
+            margin-top: rem(20rem);
+			.i-maintop{
+				line-height: rem(60rem);
+				font-size: rem(22rem);
+				background: url(../assets/indexborder.png) no-repeat center bottom;
+  				background-size: 53% rem(3rem);
+  				margin-bottom: rem(20rem);
+  				img{
+  					width: rem(2rem);
+  					height: rem(18rem);
+  					margin: 0 3%;
+  				}
+			}
             .i-maintext {
-                width: 83.8%;
-                margin-left: 8.6%;
+                width: 86%;
+                margin-left: 7%;
                 letter-spacing: rem(0.4rem);
-                font-size: $font12;
-                line-height: rem(22rem);
+                font-size: $font14;
+                line-height: rem(30rem);
                 color: #000;
                 text-align: center;
             }
@@ -101,8 +103,7 @@
             .i-maincen {
                 width: 53%;
                 margin-left: 23.5%;
-                margin-top: rem(2rem);
-
+                margin-top: rem(12rem);
                 .i-mline {
                     width: 100%;
                     height: 0.03rem;
@@ -111,89 +112,28 @@
                 .i-micon {
                     width: 0.96rem;
                     height: 0.48rem;
-                    margin-top: rem(18rem);
+                    margin-top: rem(30rem);
                     display: block;
                     margin-left: 40%;
                 }
 
             }
-            .i-maindocter {
-                width: 82%;
-                height: rem(86rem);
-                box-shadow: 0 rem(1rem) rem(2rem) #e2e1df;
-                border-radius: 0.16rem;
-                margin-left: 13.3%;
-                position: relative;
-                background: #fff;
-                margin-top: rem(18rem);
-
-                span {
-                    margin-left: 16%;
-                    line-height: rem(86rem);
-                    font-size: rem(14rem);
-                }
-
-                .i-maintest {
-                    width: 23.2%;
-                    height: 0.75rem;
-                    line-height: 0.75rem;
-                    color: #ce8654;
-                    border-radius: 0.11rem;
-                    text-align: center;
-                    position: absolute;
-                    right: 3.9%;
-                    top: 0.8rem;
-
-                    img {
-                        width: 1.92rem;
-                        height: 0.75rem;
-                    }
-
-                }
-                .i-mainportrait {
-                    width: rem(75rem);
-                    height: rem(75rem);
-                    border-radius: 50%;
-                    position: absolute;
-                    left: rem(-37.5rem);
-                    top: rem(5rem);
-
-                    img {
-                        width: rem(75rem);
-                        height: rem(75rem);
-                    }
-
-                }
-
-            }
-            .i-maindocter1 {
-                margin-bottom: 0.48rem;
-            }
-
+           .i-mainTest{
+           	width: 40%;
+           	height: rem(50rem);
+           	background: url(../assets/indextest.png) no-repeat center;
+           	background-size: cover;
+           	margin: rem(20rem) 30% rem(40rem);
+           }
             .i-maintip {
+            	width: 81%;
+            	margin-left: 9.5%;
                 color: #C69B70;
                 margin-top: rem(30rem);
                 color: #c59a6f;
                 font-weight: bold;
-            }
-            .r-mbotbtn {
-                width: 84%;
-                margin: rem(20rem) 0 0 8%;
-                overflow: hidden;
-                padding-bottom: rem(10rem);
-
-                .r-mbotleft {
-                    width: 47.6%;
-                    height: rem(50rem);
-                    margin-left: 26.2%;
-
-                    img {
-                        width: 100%;
-                        height: 100%;
-                    }
-
-                }
-
+                text-align: center;
+                line-height: rem(20rem);
             }
 
         }
