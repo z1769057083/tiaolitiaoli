6ce59608647b236f293728fe2aaf19c560f8902c
xujianrj <keys203@gmail.com>
2017-07-01 10:59:40 +0800

    bug fix.

--- src/components/looks.vue
+++ src/components/looks.vue
@@ -4,41 +4,41 @@
             <dl>
                 <dt>裸眼视力</dt>
                 <dd>
-                    <p :class="{'active': eye === 0}" @click="change_active(0,'eye',$event)">好</p>
-                    <p :class="{'active': eye === 1}" @click="change_active(1,'eye',$event)">一般</p>
-                    <p :class="{'active': eye === 2}" @click="change_active(2,'eye',$event)">差</p>
+                    <p :class="{'active': eye === 1}" @click="change_active(1,'eye',$event)">好</p>
+                    <p :class="{'active': eye === 0}" @click="change_active(0,'eye',$event)">一般</p>
+                    <p :class="{'active': eye === -1}" @click="change_active(-1,'eye',$event)">差</p>
                 </dd>
             </dl>
             <dl>
                 <dt>味觉</dt>
                 <dd>
-                    <p :class="{'active': taste === 0}" @click="change_active(0,'taste',$event)">灵敏</p>
-                    <p :class="{'active': taste === 1}" @click="change_active(1,'taste',$event)">一般</p>
-                    <p :class="{'active': taste === 2}" @click="change_active(2,'taste',$event)">迟钝</p>
+                    <p :class="{'active': taste === 1}" @click="change_active(1,'taste',$event)">灵敏</p>
+                    <p :class="{'active': taste === 0}" @click="change_active(0,'taste',$event)">一般</p>
+                    <p :class="{'active': taste === -1}" @click="change_active(-1,'taste',$event)">迟钝</p>
                 </dd>
             </dl>
             <dl>
                 <dt>嗅觉</dt>
                 <dd>
-                    <p :class="{'active': smell === 0}" @click="change_active(0,'smell',$event)">灵敏</p>
-                    <p :class="{'active': smell === 1}" @click="change_active(1,'smell',$event)">一般</p>
-                    <p :class="{'active': smell === 2}" @click="change_active(2,'smell',$event)">迟钝</p>
+                    <p :class="{'active': smell === 1}" @click="change_active(1,'smell',$event)">灵敏</p>
+                    <p :class="{'active': smell === 0}" @click="change_active(0,'smell',$event)">一般</p>
+                    <p :class="{'active': smell === -1}" @click="change_active(-1,'smell',$event)">迟钝</p>
                 </dd>
             </dl>
             <dl>
                 <dt>口气</dt>
                 <dd>
-                    <p :class="{'active': tone === 0}" @click="change_active(0,'tone',$event)">清新</p>
-                    <p :class="{'active': tone === 1}" @click="change_active(1,'tone',$event)">一般</p>
-                    <p :class="{'active': tone === 2}" @click="change_active(2,'tone',$event)">不告诉你</p>
+                    <p :class="{'active': tone === 1}" @click="change_active(1,'tone',$event)">清新</p>
+                    <p :class="{'active': tone === 0}" @click="change_active(0,'tone',$event)">一般</p>
+                    <p :class="{'active': tone === -1}" @click="change_active(-1,'tone',$event)">不告诉你</p>
                 </dd>
             </dl>
             <dl>
                 <dt>听觉</dt>
                 <dd>
-                    <p :class="{'active': hear === 0}" @click="change_active(0,'hear',$event)">好</p>
-                    <p :class="{'active': hear === 1}" @click="change_active(1,'hear',$event)">一般</p>
-                    <p :class="{'active': hear === 2}" @click="change_active(2,'hear',$event)">差</p>
+                    <p :class="{'active': hear === 1}" @click="change_active(1,'hear',$event)">好</p>
+                    <p :class="{'active': hear === 0}" @click="change_active(0,'hear',$event)">一般</p>
+                    <p :class="{'active': hear === -1}" @click="change_active(-1,'hear',$event)">差</p>
                 </dd>
             </dl>
         </div>
