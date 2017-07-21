f9e8b8963328f176558fb784cf28e3a592c4ec0c
xujianrj <keys203@gmail.com>
2017-07-21 15:06:13 +0800

    bug fix.

--- src/pages/bindBarCode.vue
+++ src/pages/bindBarCode.vue
@@ -104,7 +104,7 @@
             saoCode(){
             	var that = this;
             	wx.scanQRCode({
-				    needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+				    needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
 				    scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
 				    success: function (res) {
 //				    	that.code = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
