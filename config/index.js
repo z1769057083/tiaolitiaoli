0279a01bb53a7066c633606f0d67169043b512a7
xujianrj <keys203@gmail.com>
2017-07-25 10:06:01 +0800

    remove production source map

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    productionSourceMap: true,
+    productionSourceMap: false,//Note:xujian change it to false as do not use source map.
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
